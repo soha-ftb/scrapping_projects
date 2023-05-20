01c6167cbe6332cacde727afc7ed79fdebec6d84
soha <soha_elsayed_86@hotmail.com>
2023-05-20 11:13:49 +0200

    set base URL and file setup

--- scrapping_wuzzuf_dataAnlystJob.py
+++ scrapping_wuzzuf_dataAnlystJob.py
@@ -1,2 +1,6 @@
 from bs4 import BeautifulSoup as bs
 import requests
+
+base_url = "https://wuzzuf.net/search/jobs/?a=hpb&q=data%20analyst&start=0"
+file_1 = open("wuzzuf.csv", "w", encoding="utf-8")
+file_1.write("job_title,company_name,job_type\n")
