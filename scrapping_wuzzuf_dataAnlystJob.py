260b98411c064fbaf2bca206943b06fb2ecc9e1f
soha <soha_elsayed_86@hotmail.com>
2023-05-20 11:21:56 +0200

    Close the file

--- scrapping_wuzzuf_dataAnlystJob.py
+++ scrapping_wuzzuf_dataAnlystJob.py
@@ -17,3 +17,5 @@ for page in range(1, 12):
         c_name = container.find("a", {"class": "css-17s97q8"}).text.strip()
         j_type = container.find("a", {"class": "css-n2jc4m"}).text.strip()
         file_1.write(j_title + "," + c_name + "," + j_type + "\n")
+
+file_1.close()
